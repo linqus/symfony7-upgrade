5e4da500fdeab5d2fc99cbcd6cdcf053ff456be3
Karol Linkiewicz <karol@linkiewicz.pl>
2024-02-13 14:40:17 +0100

    asset mapper

--- assets/app.js
+++ assets/app.js
@@ -6,9 +6,12 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fontsource-variable/roboto-condensed/index.min.css';
 import './styles/app.css';
 
 // start the Stimulus application
-import './bootstrap';
+import './bootstrap.js';
 
 console.log('Hi! My name is app.js!');
